8b0be5d53a611051c83470a22289c2fbb40cd922
teknovipyazilim <37733016+teknovipyazilim@users.noreply.github.com>
2019-08-09 00:24:42 +0300

    Update teknoparasut.php

--- teknoparasut.php
+++ teknoparasut.php
@@ -1,1 +1,17 @@
 <?php
+/**
+ * Plugin Name: Tekno Paraşüt
+ * Plugin URI: http://teknovip.net/teknoparasut
+ * Description: ilanavrupam.com sitesi için PAraşüt Entegrasyonu. Yazan ahmethamdibayrak@hotmail.com
+ * Version: 0.0.1
+ * Author: teknovip
+ * Author URI: https://teknovip.net
+ * Copyright: © 2018 teknovip.net
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: teknovip
+ * Domain Path: /languages
+ *
+ * Copyright (c) 2018 teknovip.net.
+**/
+include_once('functions.php');
